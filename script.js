f0e0ecf02f8571d7e117c060e8b10f90ecb22cde
berger <robertberger.dev@gmail.com>
2022-12-13 20:35:20 -0500

    Added/Tested sentence split

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 function stringCounter(sentence){
-    // FUNCTION CODE GOES HERE
+    sentence = sentence.split(" ");
+    console.log(sentence);
 }
 
 // TEST CASE 1
-let sentence1 = "The largest living thing on earth is a giant sequoia named General Sherman";
-stringCounter(sentence1);
+// let sentence1 = "The largest living thing on earth is a giant sequoia named General Sherman";
+// stringCounter(sentence1);
 
 // TEST CASE 2
-let sentence2 = "A sunset on Mars is blue";
-stringCounter(sentence2);
+// let sentence2 = "A sunset on Mars is blue";
+// stringCounter(sentence2);
+
+stringCounter("This is a Test Sentence");
