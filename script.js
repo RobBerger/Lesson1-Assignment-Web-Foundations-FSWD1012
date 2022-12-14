71551b40a965c725f16b7779c9616d1bbb975632
berger <robertberger.dev@gmail.com>
2022-12-13 19:42:22 -0500

    Added starter code to the JS file creating a function named stringCounter

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Test");
+function stringCounter(sentence){
+    // FUNCTION CODE GOES HERE
+}
+
+// TEST CASE 1
+let sentence1 = "The largest living thing on earth is a giant sequoia named General Sherman";
+stringCounter(sentence1);
+
+// TEST CASE 2
+let sentence2 = "A sunset on Mars is blue";
+stringCounter(sentence2);
