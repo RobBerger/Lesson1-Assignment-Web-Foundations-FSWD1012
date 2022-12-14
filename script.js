a25bf4a2ffa4cb142a9a41d1b4f86513cd318ea6
berger <robertberger.dev@gmail.com>
2022-12-13 21:11:48 -0500

    Ran the official test... all looks good

--- script.js
+++ script.js
@@ -8,11 +8,9 @@ function stringCounter(sentence){
 }
 
 // TEST CASE 1
-// let sentence1 = "The largest living thing on earth is a giant sequoia named General Sherman";
-// stringCounter(sentence1);
+let sentence1 = "The largest living thing on earth is a giant sequoia named General Sherman";
+stringCounter(sentence1);
 
 // TEST CASE 2
-// let sentence2 = "A sunset on Mars is blue";
-// stringCounter(sentence2);
-
-stringCounter("This is a Test Sentence");
+let sentence2 = "A sunset on Mars is blue";
+stringCounter(sentence2);
